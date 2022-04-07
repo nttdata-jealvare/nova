6a8a6acc2dd70114e5e59b4d8370b7420f4427d9
nttdata-jealvare <jesus.alvarezcoll@everis.nttdata.com>
2022-04-07 12:51:01 +0200

    Test repository completado

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Task VALUES (1, 'Test sobre la tarea SCRUMTSK00023', 'Pending');
+INSERT INTO Task VALUES (2, 'Comunicación con el cliente sobre la nueva implementación', 'In Progress');
+INSERT INTO Task VALUES (3, 'Subida a producción de la tarea SCRUMTSK00223', 'Completed');
+INSERT INTO Task VALUES (4, 'Actualización de seguridad 502_L12', 'New');
+INSERT INTO Task VALUES (5, 'Implementación DevOps sobre nuevo elemento Azure', 'New');
+INSERT INTO Task VALUES (6, 'Continuación Test Unitarios', 'In Progress');
+INSERT INTO Task VALUES (7, 'Renovación de credenciales cuenta Kubernetes', 'Pending');
+INSERT INTO Task VALUES (8, 'Actualización de CRM por vulnerabilidad 1002:122a', 'Completed');
--- src/test/java/com/nttdata/dar/nova/TaskRepositoryTest.java
+++ src/test/java/com/nttdata/dar/nova/TaskRepositoryTest.java
@@ -2,55 +2,137 @@ package com.nttdata.dar.nova;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+/**
+ * @author jalvarco
+ *
+ */
 @DataJpaTest
 public class TaskRepositoryTest {
 
 	@Autowired
 	private TaskRepository taskRepository;
 	
+	private Task externalTask;
 	private Task task;
-	private Long id = 1L;
-	private String descriptionT = "Test";
-	private String statusT = "In Progress";
-
+	
+	private final String descriptionT = "Test";
+	private final String statusT = "Pending";
+	
+	private static final Long ID = 1L;
 	
 	@BeforeEach
 	public void initialize() {
 		this.task = new Task(this.descriptionT, this.statusT);
+		this.externalTask = new Task(this.descriptionT);
 	}
 	
+	
+	/**
+	 * 
+	 */
 	@Test
-	public void createTask() {
+	public void createTask() {		
 		Task createdTask = this.taskRepository.save(this.task);
 		assertThat(createdTask.getDescription()).isEqualTo(this.task.getDescription());
 		assertThat(createdTask.getStatus()).isEqualTo(this.task.getStatus());
 	}
 	
 	@Test
+	public void getTasks() {
+		// Clean database for control the task list
+		this.taskRepository.deleteAll();
+		
+		String descMod = "Test 2";
+		String statusMod = "Completed";
+		this.externalTask.setDescription(descMod);
+		this.externalTask.setStatus(statusMod);
+		
+		List<Task> listTasks = new ArrayList<>();
+		listTasks.add(this.task);
+		listTasks.add(this.externalTask);
+		
+		this.taskRepository.save(this.task);
+		this.taskRepository.save(this.externalTask);
+		
+		assertThat(this.taskRepository.findAll().size()).isEqualTo(listTasks.size());
+	}
+	
+	// 
+	@Test
+	public void getPendingTasks() {
+		List<Task> pendingTasks = this.taskRepository.findByStatusIs("Pending");
+		for(Task t : pendingTasks) {
+			assertThat(t.getStatus()).isEqualTo("Pending");
+		}
+	}
+	
+	@Test
+	public void getCompletedTasks() {
+		List<Task> completedTasks = this.taskRepository.findByStatusIs("Completed");
+		for(Task t : completedTasks) {
+			assertThat(t.getStatus()).isEqualTo("Completed");
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
 	public void updateTask() {
 		String desc = "New Description"; String stat = "Pending";
 		
-		// Create Task
-		this.taskRepository.save(this.task);
-		
 		//Assign the ID it should have and new values
-		this.task.setId(this.id);
+		this.task.setId(ID);
 		this.task.setDescription(desc);
 		this.task.setStatus(stat);
 		
-		// TODO: Hacer esto con assertEquals
-		
 		// Update Record
 		Task updatedTask = this.taskRepository.save(this.task);
-		assertThat(updatedTask.getId()).isEqualTo(this.id);
+		assertThat(updatedTask.getId()).isEqualTo(ID);
 		assertThat(updatedTask.getDescription()).isEqualTo(desc);
 		assertThat(updatedTask.getStatus()).isEqualTo(stat);
 		
 	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void deleteTask() {
+		this.taskRepository.save(this.task);
+		assertThat(this.taskRepository.count()).isNotZero();
+		
+		// Delete task by ID and then check it 
+		this.taskRepository.deleteById(ID);
+		assertThat(this.taskRepository.existsById(ID)).isNotEqualTo(true);
+	}
+	
+	@Test
+	public void deleteAllTasks() {
+		this.taskRepository.deleteAll();
+		assertThat(this.taskRepository.count()).isZero();
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void newExternalTask() {
+		Task newTask = this.taskRepository.save(this.externalTask);
+		String newState = "New";
+		
+		assertThat(newTask.getStatus()).isEqualTo(newState);
+	}
+	
 	
 }
